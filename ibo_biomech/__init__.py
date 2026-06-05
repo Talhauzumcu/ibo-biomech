c151edb15b61de1f729fb5ae884c20d73b035797
Talha <talha.uzumcu21@gmail.com>
2026-06-05 15:20:20 +0200

    Updated README.md

--- ibo_biomech/__init__.py
+++ ibo_biomech/__init__.py
@@ -2,4 +2,4 @@ from ibo_biomech.biomech_io import FileConverter
 from ibo_biomech.containers import AnalogData, ForceData, MarkerData, TrialData, Subject
 from ibo_biomech.handlers import C3DHandler, H5Handler
 
-__version__ = "0.1.21"
+__version__ = "0.1.3"
